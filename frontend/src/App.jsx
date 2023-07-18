983a19786c0c86bb45b76c6173fabc7dea6a1e9d
ahmed-farrag <ahmedfaarrag@gmail.com>
2023-07-18 21:32:19 +0300

    update structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import HomePage from "./Pages/Home/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+      hello
+    </div>
+  );
+}
+
+export default App;
